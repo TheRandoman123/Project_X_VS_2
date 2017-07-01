07a2456286b41e87be22d6c7174200463ccf9c75
TheRandoman123 <nerfsteven@gmail.com>
2017-07-01 12:38:52 -0400

    Started the program
    Startyed to work on the programming and now learning how to have all my
    varibales in one class

--- Project_X_VS_2/Project_X_VS_2/Program.cs
+++ Project_X_VS_2/Project_X_VS_2/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
+
+
 namespace Project_X_VS_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+
+            Console.WriteLine("Hello and welcome to the text adventure");
+            Console.ReadLine();
+            switch ()
+            {
+                case 1:
+
+                    break;
+
+                case 2:
+                    break;
+             
+            }
+
+
+
         }
     }
 }
---
+++ Project_X_VS_2/Project_X_VS_2/Vars.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_X_VS_2
+{
+   public class Vars
+    {
+        public string userInput = Console.ReadLine();
+    }
+}
