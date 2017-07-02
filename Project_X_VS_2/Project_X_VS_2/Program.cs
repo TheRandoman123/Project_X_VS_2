83e4549a5561086940b58db6c78b0386a045862f
TheRandoman123 <nerfsteven@gmail.com>
2017-07-01 20:13:11 -0400

    Removed File Vars.cs
    removed file, added switch, made a local class in the same namespace

--- Project_X_VS_2/Project_X_VS_2/Program.cs
+++ Project_X_VS_2/Project_X_VS_2/Program.cs
@@ -1,28 +1,44 @@
 ﻿using System;
+using System.Threading;
 
 
 
 namespace Project_X_VS_2
 {
+    public class Vars
+    {
+       public string userInput = Console.ReadLine();
+    }
+
     class Program
     {
+
+        
         static void Main(string[] args)
         {
+           
+        Console.WriteLine("Hello and welcome to the text adventure");
 
-            Console.WriteLine("Hello and welcome to the text adventure");
-            Console.ReadLine();
-            switch ()
+            Console.WriteLine("1 or 2?:");
+          
+            switch (userInput)
             {
-                case 1:
+                case "1":
+                    Console.Clear();
+                    Console.WriteLine("Decision 1 test");
+                    Thread.Sleep(300);
 
                     break;
 
-                case 2:
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("Decision 2 test");
+                    Thread.Sleep(300);
                     break;
              
             }
 
-
+            
 
         }
     }
